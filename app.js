5d4033786fe0ab58bf4718dd1d40a85288e1d6b2
Alexandru Cirjan <cirjan@mds.microsoft.com>
2021-10-24 16:52:10 +0300

    modified the execution interval

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import { CryptoAlgorithm } from './js/algorithm.js'
 
 let cryptoAlgorithm = new CryptoAlgorithm();
 
-setInterval(cryptoAlgorithm.runOnce, 1000);
+setInterval(cryptoAlgorithm.runOnce, 10000);
 
 //cryptoAlgorithm.runOnce();
