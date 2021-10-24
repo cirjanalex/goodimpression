0cddf7b14c3d4f8d182538f2b6502eeaba35dca2
Alexandru Cirjan <cirjan@mds.microsoft.com>
2021-10-24 17:01:07 +0300

    removed useless file and added some comments

--- js/algorithm.js
+++ js/algorithm.js
@@ -5,14 +5,16 @@ var api = new Api(apiKey || '61531a36ddf621d59e852734');
 
 export class CryptoAlgorithm {
     async runOnce() {
-        // Retrieves the current estimated value of a team together with the quantity of each symbol
+        
         var account = await api.account();
         console.log(account);
-
+        // Find a coin which is not USD
         var nonUSDTCoinToSell = account.symbols.find(p => p.quantity > 0 && p.name != "USDT");
-        if (nonUSDTCoinToSell !== undefined) {
+        // If we find one we sell it
+        if (nonUSDTCoinToSell !== undefined) {        
             await api.order({ symbol: nonUSDTCoinToSell.name, side: 'SELL', quantity: nonUSDTCoinToSell.quantity });
         }
+        // If we don't we buy one
         else {
             var prices = await api.prices();
             prices = prices.filter(p => p.name !== "USDT");
